51a8f7d91c557f63875dd7fbdd90731f44e54547
kamalogudah <mcpaul2058@gmail.com>
2023-01-18 10:08:22 +0300

    Initial Commit

---
+++ lib/mpesa_sdk/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MpesaSdk
+  VERSION = "0.1.0"
+end
---
+++ spec/mpesa_sdk_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MpesaSdk do
+  it "has a version number" do
+    expect(MpesaSdk::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
